016f0180413f3c9aad9d2e414397fb1735acd021
levon@evndev.com <levonshahnazaryan@gmail.com>
2022-02-23 01:09:31 +0400

    Բազմապատկման աղյուսակ

--- CSharp-Lessons/Lesson-04/Program.cs
+++ CSharp-Lessons/Lesson-04/Program.cs
@@ -35,6 +35,47 @@ namespace Lesson_04
                     Console.WriteLine("Not Found Error Code");
                     break;
             }
+
+
+            Console.WriteLine("Please enter the number");
+            string num = Console.ReadLine();
+            int.TryParse(num, out int intNum);
+            if (intNum > 0)
+            {
+                int firstintNum = intNum;
+                int a = 0;
+                int b = 1992;
+                while (a < b)
+                {
+                    if (intNum > 5)
+                        intNum = b;
+                    else if (intNum < a)
+                        intNum = a;
+
+                    if (firstintNum != intNum)
+                        break;
+                    a++;
+                }
+
+                Console.WriteLine($"Your {firstintNum} number between in {0} and {b}");
+            }
+            else
+            {
+                Console.WriteLine("Wrong number");
+            }
+
+            int j = 1;
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.WriteLine($"{j} x {i} = {j * i}");
+                if (i == 9)
+                {
+                    j++;
+                    i = 0;
+                }
+                if (j == 10)
+                    break;
+            }
         }
     }
 }
