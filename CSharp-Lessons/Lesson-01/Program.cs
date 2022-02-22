948625e8219b02ddcd527505dbe1480e3910041c
levon@evndev.com <levonshahnazaryan@gmail.com>
2022-02-22 22:40:05 +0400

    add change

--- CSharp-Lessons/Lesson-01/Program.cs
+++ CSharp-Lessons/Lesson-01/Program.cs
@@ -6,7 +6,19 @@ namespace Lesson_01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, can I help you?");
+            Console.ReadKey();
+
+            Console.WriteLine("What the Model of car you want to buy?");
+            string model = Console.ReadLine();
+
+            Console.WriteLine("Can you tell me price?");
+            string price = Console.ReadLine();
+
+            Console.WriteLine("Ok, please confirm the order");
+            Console.ReadKey();
+
+            Console.WriteLine($"Your car Model is {model}, please pay the {price}$");
         }
     }
 }
