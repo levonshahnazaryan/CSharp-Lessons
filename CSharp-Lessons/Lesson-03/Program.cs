cdbe993546c8b643c5e968e80da537e2a23726db
levon@evndev.com <levonshahnazaryan@gmail.com>
2022-02-23 00:00:28 +0400

    add lesson 4

---
+++ CSharp-Lessons/Lesson-03/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Lesson_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 10;
+            Console.WriteLine($"a++: {a++}");
+            Console.WriteLine($"b--: {b--}");
+
+
+            int dayOfWeek = 5;
+            if (dayOfWeek == 1)
+            {
+                Console.WriteLine("Monday");
+            }
+            else if (dayOfWeek == 2)
+            {
+                Console.WriteLine("Tuesday");
+            }
+            else if (dayOfWeek == 3)
+            {
+                Console.WriteLine("Wednesday");
+            }
+            else if (dayOfWeek == 4)
+            {
+                Console.WriteLine("Thursday");
+            }
+            else if (dayOfWeek == 5)
+            {
+                Console.WriteLine("Friday");
+            }
+            else if (dayOfWeek == 6)
+            {
+                Console.WriteLine("Saturday");
+            }
+            else
+            {
+                Console.WriteLine("Sunday");
+            }
+        }
+    }
+}
---
+++ CSharp-Lessons/Lesson-04/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Lesson_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the http code");
+            string httpCode = Console.ReadLine();
+            switch (httpCode)
+            {
+                case "400":
+                    Console.WriteLine("400 Bad Request");
+                    break;
+                case "401":
+                    Console.WriteLine("401 Unauthorized");
+                    break;
+                case "402":
+                    Console.WriteLine("402 Payment Required");
+                    break;
+                case "403":
+                    Console.WriteLine("403 Forbidden");
+                    break;
+                case "404":
+                    Console.WriteLine("404 Not Found");
+                    break;
+                case "405":
+                    Console.WriteLine("405 Method Not Allowed");
+                    break;
+                case "406":
+                    Console.WriteLine("406 Not Acceptable");
+                    break;
+                default:
+                    Console.WriteLine("Not Found Error Code");
+                    break;
+            }
+        }
+    }
+}
