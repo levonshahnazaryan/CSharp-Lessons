be66c20b7e5f21117e74eb10e23ef5cbaabb55a4
levon@evndev.com <levonshahnazaryan@gmail.com>
2022-02-27 16:29:28 +0400

    add tasks

--- CSharp-Lessons/Lesson-04/Program.cs
+++ CSharp-Lessons/Lesson-04/Program.cs
@@ -6,76 +6,96 @@ namespace Lesson_04
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter the http code");
-            string httpCode = Console.ReadLine();
-            switch (httpCode)
-            {
-                case "400":
-                    Console.WriteLine("400 Bad Request");
-                    break;
-                case "401":
-                    Console.WriteLine("401 Unauthorized");
-                    break;
-                case "402":
-                    Console.WriteLine("402 Payment Required");
-                    break;
-                case "403":
-                    Console.WriteLine("403 Forbidden");
-                    break;
-                case "404":
-                    Console.WriteLine("404 Not Found");
-                    break;
-                case "405":
-                    Console.WriteLine("405 Method Not Allowed");
-                    break;
-                case "406":
-                    Console.WriteLine("406 Not Acceptable");
-                    break;
-                default:
-                    Console.WriteLine("Not Found Error Code");
-                    break;
-            }
+            //Console.WriteLine("Please enter the http code");
+            //string httpCode = Console.ReadLine();
+            //switch (httpCode)
+            //{
+            //    case "400":
+            //        Console.WriteLine("400 Bad Request");
+            //        break;
+            //    case "401":
+            //        Console.WriteLine("401 Unauthorized");
+            //        break;
+            //    case "402":
+            //        Console.WriteLine("402 Payment Required");
+            //        break;
+            //    case "403":
+            //        Console.WriteLine("403 Forbidden");
+            //        break;
+            //    case "404":
+            //        Console.WriteLine("404 Not Found");
+            //        break;
+            //    case "405":
+            //        Console.WriteLine("405 Method Not Allowed");
+            //        break;
+            //    case "406":
+            //        Console.WriteLine("406 Not Acceptable");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Not Found Error Code");
+            //        break;
+            //}
 
 
-            Console.WriteLine("Please enter the number");
-            string num = Console.ReadLine();
-            int.TryParse(num, out int intNum);
-            if (intNum > 0)
-            {
-                int firstintNum = intNum;
-                int a = 0;
-                int b = 1992;
-                while (a < b)
-                {
-                    if (intNum > 5)
-                        intNum = b;
-                    else if (intNum < a)
-                        intNum = a;
+            //Console.WriteLine("Please enter the number");
+            //string num = Console.ReadLine();
+            //int.TryParse(num, out int intNum);
+            //if (intNum > 0)
+            //{
+            //    int firstintNum = intNum;
+            //    int a = 0;
+            //    int b = 1992;
+            //    while (a < b)
+            //    {
+            //        if (intNum > 5)
+            //            intNum = b;
+            //        else if (intNum < a)
+            //            intNum = a;
 
-                    if (firstintNum != intNum)
-                        break;
-                    a++;
-                }
+            //        if (firstintNum != intNum)
+            //            break;
+            //        a++;
+            //    }
+            //    Console.WriteLine($"Your {firstintNum} number between in {0} and {b}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Wrong number");
+            //}
 
-                Console.WriteLine($"Your {firstintNum} number between in {0} and {b}");
-            }
-            else
-            {
-                Console.WriteLine("Wrong number");
-            }
+            //int j = 1;
+            //for (int i = 1; i <= 9; i++)
+            //{
+            //    Console.WriteLine($"{j} x {i} = {j * i}");
+            //    if (i == 9)
+            //    {
+            //        j++;
+            //        i = 0;
+            //    }
+            //    if (j == 10)
+            //        break;
+            //}
 
-            int j = 1;
-            for (int i = 1; i <= 9; i++)
-            {
-                Console.WriteLine($"{j} x {i} = {j * i}");
-                if (i == 9)
-                {
-                    j++;
-                    i = 0;
-                }
-                if (j == 10)
-                    break;
-            }
+            //double a = 1;
+            //for (int i = 100; i < 1000; i++)
+            //{
+            //    if (i % 3 == 1 && i % 4 == 2)
+            //    {
+            //        a *= i;
+            //    }
+            //}
+
+            //Console.WriteLine(a);
+
+
+            string[] aaa = new string[4] { "1235", "46462", "346464", "422" };
+            var xxx = aaa.GetValue(0);
+
+            int ccc = aaa.GetLength(0);
+
+
+            //jegget array
+
         }
     }
 }
---
+++ CSharp-Lessons/Lesson-08/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Lesson_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task-01
+            WelcomeUser();
+
+            //Task-02
+            Console.Write("Please input a name: ");
+            string name = Console.ReadLine();
+            WriteUserName(name);
+
+            //Task-03
+            Console.Write("Enter a number: ");
+            string num1 = Console.ReadLine();
+            Console.Write("Enter another number: ");
+            string num2 = Console.ReadLine();
+            WriteSumTwoNumbers(num1, num2);
+        }
+
+        #region Task-01
+        public static void WelcomeUser()
+        {
+            Console.WriteLine("Welcome Friends!");
+            Console.WriteLine("Have a nice day!\n");
+        }
+        #endregion
+        #region Task-02
+        public static void WriteUserName(string name)
+        {
+            Console.WriteLine($"Welcome friend {name}!");
+            Console.WriteLine("Have a nice day!\n");
+        }
+        #endregion
+        #region Task-03
+        public static void WriteSumTwoNumbers(string num1, string num2)
+        {
+            int.TryParse(num1, out int _num1);
+            int.TryParse(num2, out int _num2);
+            Console.WriteLine($"The sum of two numbers is: {_num1 + _num2}");
+        }
+        #endregion
+
+    }
+}
