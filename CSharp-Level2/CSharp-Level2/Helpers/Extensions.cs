1708b24acb4cf64a92525d25443b1a6161ee4500
levon@evndev.com <levonshahnazaryan@gmail.com>
2022-03-13 14:59:46 +0400

    add Insertion sort

--- CSharp-Level2/CSharp-Level2/Helpers/Extensions.cs
+++ CSharp-Level2/CSharp-Level2/Helpers/Extensions.cs
@@ -23,6 +23,43 @@
             return array;
         }
 
-        https://www.youtube.com/watch?v=cHOycxTvEm0
+        //https://www.youtube.com/watch?v=cHOycxTvEm0
+        public static int[] Selection_Sort(int[] array)
+        {
+            int item = 0;
+            while (item < array.Length)
+            {
+                int minIndex = item;
+
+                for (int i = item; i < array.Length; i++)
+                {
+                    if (array[item] > array[i])
+                        if (array[i] < array[minIndex])
+                            minIndex = i;
+                }
+
+                int first = array[item];
+                array[item] = array[minIndex];
+                array[minIndex] = first;
+                item += 1;
+            }
+            return array;
+        }
+
+        //https://www.youtube.com/watch?v=jvroKFOdF5Y
+        public static int[] Insertion_Sort(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                while (i > 0 && array[i - 1] > array[i])
+                {
+                    int first = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = first;
+                    i -= 1;
+                }
+            }
+            return array;
+        }
     }
 }
--- CSharp-Level2/CSharp-Level2/Program.cs
+++ CSharp-Level2/CSharp-Level2/Program.cs
@@ -7,11 +7,27 @@ namespace CSharp_Level2
     {
         static void Main(string[] args)
         {
-            int[] array = { 3, 8, 5, 2, 4, 1, 7, 6, 9 };
-
             //Bubble Sort
-            var bubble = Extensions.Bubble_Sort(array);
+            int[] arrayBubble = { 3, 8, 5, 2, 4, 1, 7, 6, 9 };
+            Console.Write("Bubble Sort: ");
+            var bubble = Extensions.Bubble_Sort(arrayBubble);
             foreach (var item in bubble)
+                Console.Write($"{item} ");
+
+            Console.WriteLine("");
+            //Selection Sort
+            int[] arraySelection = { 3, 8, 5, 2, 4, 1, 7, 6, 9 };
+            Console.Write("Selection Sort: ");
+            var selecttion = Extensions.Selection_Sort(arraySelection);
+            foreach (var item in selecttion)
+                Console.Write($"{item} ");
+
+            Console.WriteLine("");
+            //Insertion Sort
+            int[] arrayInsertion = { 3, 8, 5, 2, 4, 1, 7, 6, 9 };
+            Console.Write("Insertion Sort: ");
+            var insertion = Extensions.Insertion_Sort(arrayInsertion);
+            foreach (var item in insertion)
                 Console.Write($"{item} ");
 
 
