d337d5b8eb09b4a74ccb722b3a2223143f97f73d
levon@evndev.com <levonshahnazaryan@gmail.com>
2022-03-27 21:12:38 +0400

    collection test

--- CSharp-Level2/Lesson-03/Program.cs
+++ CSharp-Level2/Lesson-03/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace Lesson_03
 {
@@ -12,11 +13,37 @@ namespace Lesson_03
             hashtable.Add("book2", "Lesson2");
             hashtable.Add("book3", "Lesson3");
 
+            hashtable["book1"] = "Hello";
+            hashtable.Remove("book2");
+
 
             Console.WriteLine(hashtable["book1"]);
             Console.WriteLine(hashtable["book2"]);
             Console.WriteLine(hashtable["book3"]);
 
+            hashtable.Clear();
+
+
+            List<int?> aa = new List<int?>();
+            aa.Add(null);
+
+
+
+            SortedList<string, string> fffff = new();
+            fffff.Add("vaaaa", "aaaa");
+            fffff.Add("baaa", "aaaa");
+            fffff.Add("caaa", "aaaa");
+            fffff.Add("daaa", "aaaa");
+
+            LinkedList<string> aaaa = new();
+
+            aaaa.AddFirst("1");
+            aaaa.AddFirst("2");
+            aaaa.AddLast("88");
+            aaaa.AddLast("99");
+            aaaa.AddFirst("3");
+            aaaa.AddFirst("4");
+            aaaa.AddFirst("5");
         }
     }
 }
