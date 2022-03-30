b1b96a628349c88765ed1f88e4b3d52f209f3e8f
levon@evndev.com <levonshahnazaryan@gmail.com>
2022-03-30 20:18:11 +0400

    ref out in

---
+++ CSharp-Level2/Lesson-04/Helpers/RefOutInRep.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lesson_04.Helpers
+{
+    public class RefOutInRep
+    {
+        public static void ConsRef(ref int number)
+        {
+            number *= 6;
+            Console.WriteLine(number);
+        }
+        public static void ConsIn(in int number)
+        {
+            //number = 44; error readonly
+            Console.WriteLine(number);
+        }
+        public static void ConsOut(out int number)
+        {
+            number = 44;
+            Console.WriteLine(number);
+        }
+        public static void ConsHello(ref string charik)
+        {
+            Console.WriteLine(charik);
+            charik = "Good Bye";
+        }
+    }
+}
---
+++ CSharp-Level2/Lesson-04/Program.cs
@@ -0,0 +1,21 @@
+﻿using Lesson_04.Helpers;
+using System;
+
+namespace Lesson_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 4;
+            RefOutInRep.ConsRef(ref num);
+            RefOutInRep.ConsIn(in num);
+            RefOutInRep.ConsOut(out num);
+            Console.WriteLine(num);
+
+            string charik = "Hello Friend";
+            RefOutInRep.ConsHello(ref charik);
+            Console.WriteLine(charik);
+        }
+    }
+}
